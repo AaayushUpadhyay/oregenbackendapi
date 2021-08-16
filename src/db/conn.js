9489fd0ab5e72ce4f47ff44d598d0db540f8a615
Ayush Upadhyay <upadhyayayush36@gmail.com>
2021-08-16 13:32:23 +0530

    seventh commit

--- src/db/conn.js
+++ src/db/conn.js
@@ -3,7 +3,7 @@ const validator=require('validator');
 // connecting to server
 // process.env.MONGODB_URI
 
-mongoose.connect("mongodb://127.0.0.1:27017/userdb",{ useUnifiedTopology: true,useNewUrlParser: true ,useCreateIndex:true , useFindAndModify:false});
+mongoose.connect(process.env.MONGODB_URI,{ useUnifiedTopology: true,useNewUrlParser: true ,useCreateIndex:true , useFindAndModify:false});
 mongoose.connection
 .once('open',()=>console.log("connected")
 )
